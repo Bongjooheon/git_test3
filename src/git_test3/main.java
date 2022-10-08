066e8aa405de945ba8a0ffaa7538800bdce76f60
Bongjooheon <ipo5069@naver.com>
2022-10-08 19:34:14 +0900

    최초커

--- src/git_test3/main.java
+++ src/git_test3/main.java
@@ -1,8 +1,13 @@
 package git_test3;
 
+import java.util.Arrays;
+
 public class main {
-	
+
 	public static void main(String[] args) {
-		new hi().say();
+		int[] a = {4, 8, 2, 6, 10, 3, 1, 7, 9, 5};
+		Sort.sort(a);
+		System.out.println(Arrays.toString(a));
 	}
+
 }
